7a5923000cc1d4396c036ed0f5ebbbda06f568a4
Martin <martinwjwilson@gmail.com>
2023-07-17 21:21:48 +0100

    Added Player class to represent a player as an object

---
+++ Player.py
@@ -0,0 +1,19 @@
+class Player:
+    id: str
+    avatar: str
+    country: str
+    followers: int
+    is_streamer: bool
+    joined: int
+    last_online: int
+    league: str  # could be a class/struct
+    location: str
+    name: str
+    player_id: int
+    status: str  # check this
+    url: str
+    username: str
+    verified: bool
+
+    # TODO: Create method to return join date
+    # TODO: Create method to return last online as date
