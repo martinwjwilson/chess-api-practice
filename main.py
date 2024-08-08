a374a1ffadc638d46cd426c40b141cb212ca9340
Martin <martinwjwilson@gmail.com>
2024-08-09 00:46:41 +0100

    Change client to bot
    Change the word client to bot as this follows the standard in the documentation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pprint
 import json
 import typing
-
 from networking import Networking
 import discord
 from discord.ext import commands
@@ -15,23 +14,23 @@ with open("token.json", 'r') as f:
 # intents
 intents = discord.Intents.default()
 intents.message_content = True
-client = commands.Bot(command_prefix=">", intents=intents)
+bot = commands.Bot(command_prefix=">", intents=intents)
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f"{client.user.name} - {client.user.id}")  # name of bot and ID
+    print(f"{bot.user.name} - {bot.user.id}")  # name of bot and ID
     print(discord.__version__)  # current version of discord.py
     print("Ready...")
 
 
-@client.command()
+@bot.command()
 async def hello(ctx):
     print("Someone said hello")
     await ctx.send("Hi there :3")
 
 
-@client.command()
+@bot.command()
 async def player_rating(ctx, username: typing.Optional[str] = "", game_type: typing.Optional[str] = ""):
     # ask for username if one wasn't provided
     if username == "":
@@ -54,27 +53,27 @@ async def player_rating(ctx, username: typing.Optional[str] = "", game_type: typ
     # TODO: Convert input to possible game types
 
     print(f"username is now {username}")
-    network_client = Networking()
-    rating = network_client.get_player_rating(player_username=username, game_type=game_type)
+    network_bot = Networking()
+    rating = network_bot.get_player_rating(player_username=username, game_type=game_type)
     await ctx.send(f"The rating for **{username}** in **{game_type}** is **{rating}**")
 
 
 async def get_input_from_question(ctx, question: str) -> str:
     await ctx.send(question)
-    msg = await client.wait_for('message',
+    msg = await bot.wait_for('message',
                                 check=lambda message: message.author == ctx.author)
     return msg.content
 
 
 # TODO: Implement set_player function
-# @client.command()
+# @bot.command()
 # async def set_player(ctx):
 #     await ctx.send("What is your username on chess.com?")
-#     message = await client.wait_for('message')
+#     message = await bot.wait_for('message')
 #     # await print_leaderboards(ctx)
 #     print(Networking.test_method())
 #     await ctx.send(f"You are now linked to: {message.content}")
 
 
 if __name__ == '__main__':
-    client.run(token)
+    bot.run(token)
