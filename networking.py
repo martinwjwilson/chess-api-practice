81c104aa20a00b81fc6845f055ba53b65d477f06
Martin <martinwjwilson@gmail.com>
2024-08-11 21:14:03 +0100

    Clean up get player method
    Split the method up, creating a generic method to perform network requests with the new header

--- networking.py
+++ networking.py
@@ -1,11 +1,15 @@
+import pprint
 import requests
+
+printer = pprint.PrettyPrinter()
 
 
 class Networking:
 
-    @staticmethod
-    def get_player(player_username: str):
-        response = requests.get(f'https://api.chess.com/pub/player/{player_username}').json()
+    def get_player(self, player_username: str):
+        url = f'https://api.chess.com/pub/player/{player_username}'
+        response = self._perform_request(url=url)
+        printer.pprint(response)
         return response
 
     @staticmethod
@@ -14,3 +18,7 @@ class Networking:
         player_rating = response[game_type]['last']['rating']
         print(f"Your rating is: {player_rating}")
         return int(player_rating)
+
+    @staticmethod
+    def _perform_request(url):
+        return requests.get(url, headers={'User-Agent': 'username: p1u95, email: martinwjwilson@gmail.com'}).json()
