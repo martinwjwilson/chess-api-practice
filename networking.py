ba6a2eeba2308f5dbc08c70f1249840a04680b4c
Martin <martinwjwilson@gmail.com>
2024-08-12 17:17:03 +0100

    Add method to get a list of game archives

--- networking.py
+++ networking.py
@@ -20,6 +20,15 @@ class Networking:
         printer.pprint(f"Your rating for {game_type} is: {player_rating}")
         return int(player_rating)
 
+    def get_player_game_archives(self, player_username: str):
+        """
+        Returns a list of game archives
+        """
+        url = f'https://api.chess.com/pub/player/{player_username}/games/archives'
+        response = self.__perform_request(url=url)
+        # TODO: Error handling - Put a check in here for if the response fails
+        return games
+
     @staticmethod
     def __perform_request(url):
         return requests.get(url, headers={'User-Agent': 'username: p1u95, email: martinwjwilson@gmail.com'}).json()
